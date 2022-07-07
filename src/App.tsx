ab1ba50e37618957ee10d8ec30af2d6dd35afc70
Martin Frajkor <martin.frajkor@student.tuke.sk>
2022-07-07 18:08:25 +0200

    added basic theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React, { useEffect } from "react";
 import { useAppSelector, useAppDispatch} from "./store/hooks";
 import { RootState, AppDispatch } from "./store/store";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme} from "@mui/material";
 
 import { Button } from "@mui/material";
 
-function App() {
+const theme = createTheme({
+  palette: {
+      primary: {
+        main: '#3550b3',
+      },
+      secondary: {
+        main: '#d47a00',
+      },
+      error: {
+        main: '#f53325',
+      },
+    },
+});
+
+const App = () => {
   const state: RootState = useAppSelector((state: RootState) => state);
   const dispatch: AppDispatch = useAppDispatch()
 
@@ -13,12 +29,15 @@ function App() {
   }, [state]);
 
   return (
-    <div>
+    <>
+    <CssBaseline/>
+    <ThemeProvider theme={theme}>
       <Button size="large" onClick={() => {dispatch({type: "ADD_TODO", payload: {id: "1", todoItems: []}})}}>ADD TODO LIST</Button>
       <Button size="large" onClick={() => {dispatch({type: "REMOVE_TODO", payload: {id: "1"}})}}>REMOVE TODO LIST</Button>
       <Button size="large" onClick={() => {dispatch({type: "ADD_TODO_ITEM", payload: {id: "1", todoListId: "1", title: "Hello world"}})}}>ADD TODO ITEM</Button>
       <Button size="large" onClick={() => {dispatch({type: "REMOVE_TODO_ITEM", payload: {id: "1", todoListId: "1"}})}}>REMOVE TODO ITEM</Button>
-    </div>
+    </ThemeProvider>
+    </>
   );
 }
 
